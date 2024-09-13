8444d869fec05a3522ccf16477de16f0d50423a5
artem <artem@GopheR.myguest.virtualbox.org>
2024-09-13 23:19:13 +0300

    pobeda

---
+++ hi.cpp
@@ -0,0 +1,15 @@
+/*
+
+Artem Churilov st130184@student.spbu.ru
+zadacha1
+
+*/
+
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	std :: cout<<"Hello, world"<<std::endl;
+	return 0;
+}
